9143ce59b1988864178345021015a10155c13076
mkassir <med.kr.101@gmail.com>
2025-12-03 17:19:16 +0100

    feat : Create table de Accounts

--- dah.sql
+++ dah.sql
@@ -13,3 +13,16 @@ CREATE TABLE
         full_name VARCHAR(20) NOT NULL,
         email VARCHAR(50) NOT NULL
     );
+
+CREATE TABLE
+    Accounts (
+        account_id INT PRIMARY KEY AUTO_INCREMENT,
+        account_number VARCHAR(20) NOT NULL,
+        balance DECIMAL(15, 2) DEFAULT 0,
+        account_type ENUM ('Checking', 'Savings', 'Business'),
+        customer_id INT,
+        advisor_id INT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (customer_id) REFERENCES Customers (Customers_id),
+        FOREIGN KEY (advisor_id) REFERENCES Advisors (advisor_id)
+    );
