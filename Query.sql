0d522183c879afed4cbda670efb053ba58c8b12c
mkassir <med.kr.101@gmail.com>
2025-12-03 17:36:21 +0100

    feat : 1. Insérer un nouveau customer

---
+++ Query.sql
@@ -0,0 +1,10 @@
+-- 1. Insérer un nouveau customer
+INSERT INTO
+    Customers (full_name, email, phone, registration_date)
+VALUES
+    (
+        'Zakaria',
+        'zakaria@example.com',
+        '0645678901',
+        '2025-12-04'
+    );
