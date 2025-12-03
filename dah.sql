4e2cee94337207f7556d893c896f4c24dcf473a0
mkassir <med.kr.101@gmail.com>
2025-12-03 17:17:41 +0100

    feat : Create table de Customers

---
+++ dah.sql
@@ -0,0 +1,8 @@
+CREATE TABLE
+    Customers (
+        customer_id INT PRIMARY KEY AUTO_INCREMENT,
+        full_name VARCHAR(20) NOT NULL,
+        email VARCHAR(50) NOT NULL,
+        phone VARCHAR(20),
+        registration_date DATE
+    );
