652c5a01669c3dbd02e18d1cd1342fbdd6fc5be9
mkassir <med.kr.101@gmail.com>
2025-12-03 17:37:22 +0100

    feat : 2. Modifier le numéro de téléphone d’un customer

--- Query.sql
+++ Query.sql
@@ -8,3 +8,10 @@ VALUES
         '0645678901',
         '2025-12-04'
     );
+
+-- 2. Modifier le numéro de téléphone d’un customer
+UPDATE Customers
+SET
+    phone = '0656789012'
+WHERE
+    customer_id = 1;
