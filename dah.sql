1d5971e89e97ef2ca083689ca45d0a8f9cbc7f5e
mkassir <med.kr.101@gmail.com>
2025-12-03 17:18:26 +0100

    feat : Create table de Advisors

--- dah.sql
+++ dah.sql
@@ -6,3 +6,10 @@ CREATE TABLE
         phone VARCHAR(20),
         registration_date DATE
     );
+
+CREATE TABLE
+    Advisors (
+        advisor_id INT PRIMARY KEY AUTO_INCREMENT,
+        full_name VARCHAR(20) NOT NULL,
+        email VARCHAR(50) NOT NULL
+    );
